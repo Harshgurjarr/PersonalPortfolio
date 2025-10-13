209ca57052aaf7b625f6fe1e1d21b70bbda806c1
Harshgurjarr <harshgurjar590@gmail.com>
2025-10-13 19:36:46 +0530

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from 'node:url'
+
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
